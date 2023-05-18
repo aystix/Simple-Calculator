50861477c6ce9b6e9e0dcc7df1b54bbbc3acdda7
adeyemiayomide <aystix2@gmail.com>
2023-05-18 08:48:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function addition(num1, num2) {
+    console.log (num1 + num2);
+}
+function subtraction(num1, num2) {
+    console.log (num1 - num2);
+}
+function calculator(num1, num2, operator) {
+    return operator (num1, num2)
+}
+addition(5 , 4);
